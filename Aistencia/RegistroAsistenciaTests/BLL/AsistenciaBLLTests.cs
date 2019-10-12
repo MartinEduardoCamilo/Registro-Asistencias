3e48986005245a8d5a7372a273b42dcd491b8850
MartinCamilo13 <47596798+MartinCamilo13@users.noreply.github.com>
2019-10-12 17:28:27 -0400

    Test sin terminar

---
+++ Aistencia/RegistroAsistenciaTests/BLL/AsistenciaBLLTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegistroAsistencia.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroAsistencia.BLL.Tests
+{
+    [TestClass()]
+    public class AsistenciaBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
